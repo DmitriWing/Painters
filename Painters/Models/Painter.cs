e8b08bd8452b5e1ffc0027fc1c30d49f2a0987e3
pupil <pupil@ivkhk.ee>
2022-09-22 15:09:41 +0300

    first initialisation

---
+++ Painters/Models/Painter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Painters.Models
+{
+    public class Painter
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Name and Surename")]
+        public string Name { get; set; }
+
+        [Display(Name = "Place of birth")]
+        public int Country { get; set; }
+
+        [Display(Name = "About")]
+        public string Biography { get; set; }
+
+        [Display(Name = "Photo")]
+        public byte[] Photo { get; set; }
+        public string PhotoType { get; set; }
+
+        [Display(Name = "Painting name")]
+        public ICollection<Painting> Paintings { get; set; }
+
+        public Painter()
+        {
+            Paintings = new List<Painting>();
+        }
+    }
+}
---
+++ Painters/Models/Painting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Painters.Models
+{
+    public class Painting
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Painting Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Year of creation")]
+        public int Year { get; set; }
+
+        [Display(Name = "Storing place")]
+        public string Museum { get; set; }
+
+        [Display(Name = "Canvas")]
+        public byte[] Canvas { get; set; }
+
+        public string CanvasType { get; set; }
+
+        public int? PainterId { get; set; }
+
+        public Painter Painter { get; set; }
+
+    }
+}
